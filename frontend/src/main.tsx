2a742a4a89a797c0d02186db5f23726a17bc66eb
michael-zanaglia-lapi <michael@lapigroupe.fr>
2025-01-20 15:18:08 +0100

    update

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,19 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter as Router, Route, Routes } from 'react-router';
 import './index.css'
 import App from './App.tsx'
 import Provider from '@/components/ui/provider.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <Router>
+    
     <Provider>
-      <App />
+      
+      <Routes>
+        <Route index path='/' element={<App/>} />
+      </Routes>
+
     </Provider>
-  </StrictMode>
+
+  </Router>
 )
